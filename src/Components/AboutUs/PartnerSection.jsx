d570e8310abdf5b347ca68ddcca8e619c1cff652
Shubham Kumar <sk6930037@gmail.com>
2024-12-21 15:47:14 +0530

    2

--- src/Components/AboutUs/PartnerSection.jsx
+++ src/Components/AboutUs/PartnerSection.jsx
@@ -1,4 +1,5 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import React from 'react';
 
 import PartnersBg from '../AboutUs/Images/PartnersBg.png';
