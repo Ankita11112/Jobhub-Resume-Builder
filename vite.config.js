73d861f2086496fbe295316c6243bbc1759e7646
Shubham Kumar <sk6930037@gmail.com>
2024-12-21 17:28:45 +0530

    q

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: 'build', // Specifies the output directory
+    chunkSizeWarningLimit: 6000, // Sets the warning limit for chunk sizes
   },
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 6000,
-  },
 });
