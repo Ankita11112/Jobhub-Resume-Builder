ca9e43e7bb725accb336f5d713b4b20f9fe92cb9
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-02 10:25:01 +0530

    u

--- vite.config.js
+++ vite.config.js
@@ -14,15 +14,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Changes the output directory from "dist" to "build"
-    chunkSizeWarningLimit: 6000, // Adjust chunk size warning limit
+    outDir: "build",
+    chunkSizeWarningLimit: 6000,
   },
-  server: {
-    open: true, // Automatically opens the browser during dev
-  },
-  base: "/", // Ensure proper handling of routes; update if deploying to a subdirectory
+  base: "/", 
 });
