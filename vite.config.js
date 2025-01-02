c02fe351024f939562675ade39fd02b6decf2fe4
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-02 10:34:10 +0530

    re-built

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     // Merge all build options here
-//     outDir: 'build', // Changes the output directory from "dist" to "build"
-//     chunkSizeWarningLimit: 6000, // Adjust chunk size warning limit
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -20,5 +7,5 @@ export default defineConfig({
     outDir: "build",
     chunkSizeWarningLimit: 6000,
   },
-  base: "/", 
+  base: "/",
 });
